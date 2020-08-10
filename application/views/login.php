b6623cd4ec3a9b744d64503275aa0e950bc7a886
David Solís <daed.solis@gmail.com>
2020-08-10 12:44:32 -0500

    Add Materialize
    Use Materialize with SASS.
    
    Materialize Version: 1.0.0

--- application/views/login.php
+++ application/views/login.php
@@ -8,8 +8,13 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 			<meta charset="utf-8">
 			<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 			<title>Iniciar sesión</title>
+      
+      <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+      <link rel="stylesheet" href="assets/login.css">
 		</head>
 		<body>
 			
+			<script src="https://code.jquery.com/jquery-3.5.0.min.js"></script>
+			<script src="assets/materialize-src/js/bin/materialize.min.js"></script>
 		</body>
 </html>
