04a01d5b914ee0c9d029af0f628bb2b30130ea96
David Solís <daed.solis@gmail.com>
2020-08-10 11:53:14 -0500

    Create Login
    Basic structure for MVC

---
+++ application/views/login.php
@@ -0,0 +1,15 @@
+<?php 
+defined('BASEPATH') OR exit('No direct script access allowed'); 
+?>
+
+<!DOCTYPE html>
+	<html lang="es" dir="ltr">
+		<head>
+			<meta charset="utf-8">
+			<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+			<title>Iniciar sesión</title>
+		</head>
+		<body>
+			
+		</body>
+</html>
